443b1b742027e1137d7c6fac92e3149202462fd8
Goblin_Thug <68116856+GoblinThug@users.noreply.github.com>
2026-08-15 20:06:46 +0600

    fix apk builder

---
+++ plugins/alarm-overlay/android/src/main/java/com/goblinthug/pillsnative/overlay/AlarmOverlayFileProvider.java
@@ -0,0 +1,7 @@
+package com.goblinthug.pillsnative.overlay;
+
+import androidx.core.content.FileProvider;
+
+/** Distinct provider class so it does not clash with Capacitor's FileProvider. */
+public class AlarmOverlayFileProvider extends FileProvider {
+}
